5aca03b2d0505ee87a5da13e3856a28ffa4aed68
SAUMYA CHOUDHURI <saumya.choudhuri123@gmail.com>
2026-06-19 13:33:37 +0000

    addition of os, chromaDB, vvid

---
+++ support-agent/scripts/ingest_docx.py
@@ -0,0 +1,7 @@
+"""Run this once to ingest sample docs into ChromaDB."""
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'backend'))
+
+import chromadb
+from chromadb.utils import embedding_functions
+import vvid
